76370a21bdab99a8ba1a03ee31f2f691f902a4bf
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-09 13:28:34 +0000

    Define routes not to authenticate

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,15 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Required path """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if excluded_paths == []:
+            return True
+        if path in excluded_paths or path + \
+                '/' in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
