b23a540688ec7008fd4ebd504344ec928484b6cf
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-09 15:05:39 +0000

    create a before_request handler

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
-        return None
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Current user """
