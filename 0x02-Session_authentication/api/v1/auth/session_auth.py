4f1f132a046cdcaed802eac60f5a1db405e0af05
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-18 08:23:10 +0000

    Create the Session Auth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" A module for Session Authentication """
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ A class that handles
+        session authentication
+    """
+    pass
