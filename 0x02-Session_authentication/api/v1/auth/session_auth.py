a6e77d6542dabf092c1c8f5d975572495722fc65
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-18 09:05:02 +0000

    Implement create_session method to return Sesssion ID"
    git add .
    k
    ikhblbl
    ;

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,10 +2,19 @@
 """ A module for Session Authentication """
 
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ A class that handles
         session authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create session id """
+        if user_id is None or type(user_id) is not str:
+            return None
+        s_id = str(uuid4())
+        self.user_id_by_session_id[s_id] = user_id
+        return s_id
