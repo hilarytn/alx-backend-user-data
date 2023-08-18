17ea60a88e9331f19db311050258bcba273010c2
Hilary Titus Naor <hilarytn@gmail.com>
2023-08-18 13:58:30 +0000

    Change cookie error handler to 401

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -64,15 +64,11 @@ def before_request() -> str:
     setattr(request, "current_user", auth.current_user(request))
     if not (auth.require_auth(request.path, expath)):
         return
-
-    if (auth.authorization_header(request)) is None:
-        abort(401)
-
     if (auth.current_user(request)) is None:
         abort(403)
     if auth.authorization_header(request) and \
             auth.session_cookie(request) is None:
-        abort(404)
+        abort(401)
 
 
 if __name__ == "__main__":
